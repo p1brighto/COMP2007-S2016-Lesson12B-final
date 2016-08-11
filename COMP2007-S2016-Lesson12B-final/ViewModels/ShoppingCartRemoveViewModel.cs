48dae39dfdc8e07a4f9318713c3ac7e636ae6f3c
p1brighto <p1brighto@gmail.com>
2016-08-11 15:30:46 -0400

    Added shopping cart controllers

---
+++ COMP2007-S2016-Lesson12B-final/ViewModels/ShoppingCartRemoveViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP2007_S2016_Lesson12B_final.ViewModels
+{
+    public class ShoppingCartRemoveViewModel
+    {
+        public virtual  string Message { get; set; }
+        public virtual  decimal CartTotal { get; set; }
+        public virtual  int CartCount { get; set; }
+        public virtual  int ItemCount { get; set; }
+        public virtual  int DeleteId { get; set; }
+    }
+}
+}
---
+++ COMP2007-S2016-Lesson12B-final/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using COMP2007_S2016_Lesson12B_final.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP2007_S2016_Lesson12B_final.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public virtual List<Cart> CartItems { get; set; }
+        public virtual decimal CartTotal { get; set; }
+    }
+}
+}
